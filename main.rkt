95a64dfc211c103332d46a54552833e49a2b825a
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 15:30:04 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,17 +1,17 @@
 #lang racket
 
-; (list-index.pos pred lst pos): F X L X N -> N
+; (list.find-index.pos pred lst pos): F X L X N -> N
 ; returns pos-based position of first element in lst that satisfies predicate pred
-(define list-index.pos
+(define list.find-index.pos
   (lambda (pred lst pos)
     (if (null? lst)
         #f
         (if (pred (car lst))
             pos
-            (list-index.pos pred (cdr lst) (+ pos 1))))))
+            (list.find-index.pos pred (cdr lst) (+ pos 1))))))
 
-; (list-index pred lst): F X L -> N
+; (list.find-index pred lst): F X L -> N
 ; returns 0-based position of first element in lst that satisfies predicate pred
-(define list-index
+(define list.find-index
   (lambda (pred lst)
-    (list-index.pos pred lst 0)))
+    (list.find-index.pos pred lst 0)))
